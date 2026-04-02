763d2c1d885e91d8b6a102c6a61e05c7322bcb8b
Anujsjm14 <anujsjm14@gmail.com>
2026-04-02 11:57:19 +0530

    Code for calling API

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express')
+const axios=require('axios')
 const app=express();
 const PORT=process.env.PORT || 3000;
 app.use(express.urlencoded({extended:false}))
+
+
+//routes
 app.get("/",(req,res)=>{
     res.send("Server is running!!")
 })
@@ -12,12 +16,25 @@ app.get("/test",(req,res)=>{
     })
 })
 
-app.post("/slack",(req,res)=>{
-    const text=req.body.text;
-    if(!text){
-        return res.send("Please provide a URL!!")
+app.post("/slack", async(req,res)=>{
+    const text = req.body.text;
+    if (!text){
+        return res.send("Please provide a URL!!");
     }
-    else return res.send(`Analyzing: ${text}`)
-})
+    let url=text;
+    if (!text.startsWith("http")){
+        url="https://"+text;
+    }
+    try {
+        const response=await axios.get(
+            `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}`
+        );
+        console.log(response.data);
+        return res.send(`Analysis done for: ${url}`);
+    } catch (error){
+        console.error(error.message);
+        return res.send("Error analyzing the website");
+    }
+});
 
 app.listen(PORT,()=> console.log(`Server is up and running at ${PORT}!!`))
